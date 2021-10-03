cd22c9d676942485cf88b0fa687af07759084fad
André Ferreira <54104227+andrehck@users.noreply.github.com>
2021-10-03 10:54:06 -0300

    Add files via upload

---
+++ testsinglecirclelink2.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data string
+	next *Node
+}
+
+var head *Node = new(Node)
+var tail *Node = new(Node)
+
+func initial() {
+	head.data = "A"
+	head.next = nil
+
+	var nodeB *Node = &Node{data: "B", next: nil}
+	head.next = nodeB
+
+	var nodeC *Node = &Node{data: "C", next: nil}
+
+	nodeB.next = nodeC
+
+	tail.data = "D"
+	tail.next = head
+	nodeC.next = tail
+}
+
+func removeNode(removePosition int) {
+	var p = head
+	var i = 0
+
+	for {
+		if p.next == nil || i >= removePosition-1 {
+			break
+		}
+		p = p.next
+		i++
+	}
+
+	var temp = p.next
+	p.next = p.next.next
+	temp.next = nil
+}
+
+func output(node *Node) {
+	var p = node
+
+	for {
+		fmt.Printf("%s ->", p.data)
+		p = p.next
+		if p == head {
+			break
+		}
+	}
+	fmt.Printf("%s\n\n", p.data)
+}
+
+func main() {
+	initial()
+
+	fmt.Printf("Delete a new node E at index = 2 : \n")
+
+	removeNode(2)
+
+	output(head)
+}
