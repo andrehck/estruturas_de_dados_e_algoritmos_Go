87b78dddbbf747c9c2e4251ab2e42555e1d37843
André Ferreira <54104227+andrehck@users.noreply.github.com>
2022-11-13 09:56:50 -0300

    Update bubblesort.go

--- bubblesort.go
+++ bubblesort.go
@@ -1,32 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var score = []int{90, 70, 50, 80, 60, 85}
-	var lenght = len(score)
+	var scores = []int{1, 6, 3, 8, 4, 9, 0, 3}
+	var lenght = len(scores)
+	fmt.Println(sort(scores, lenght))
 
-	sort(score, lenght)
-
-	for i := 0; i < lenght; i++ {
-		fmt.Printf("%d ", score[i])
-	}
 }
 
-func sort(arrays []int, lenght int) {
-	for i := 0; i < lenght-1; i++ {
+func sort(array []int, lenght int) []int {
+	for i := 0; i < lenght-1; i++ { //adiciona 1 até i ser igual a 7
 		var isSwap = false
-		for j := 0; j < lenght-i-1; j++ {
-			if arrays[j] > arrays[j+1] {
-				var flag = arrays[j]
-				arrays[j] = arrays[j+1]
-				arrays[j+1] = flag
-				isSwap = true
+
+		for j := 0; j < lenght-i-1; j++ { // i=0;j=0;tamanhodoarray-i-1
+			if array[j] > array[j+1] { //comparação entre o numero do array e seu proximo
+				var flag = array[j] // se for maior a variavel flag é igual a posiçao do array com index j
+				fmt.Println(flag)
+				array[j] = array[j+1] // a posicao j é alterada pelo j+1(proximo numero)
+				fmt.Println(flag)
+				array[j+1] = flag // a posicão que seria menor é alterada pela flag
+				fmt.Println(flag)
+				isSwap = true // continua realizando a troca
 			}
 		}
 		if isSwap == false {
 			break
 		}
 	}
-
+	return array
 }
